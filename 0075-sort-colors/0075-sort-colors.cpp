95090efa9853abd88933bcb888c5065d3af249fb
Sambram S S <sambramss7@gmail.com>
2026-07-10 13:33:57 +0530

    Time: 0 ms (100%), Space: 11.5 MB (78.94%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int zero=0;
+        int one=0;
+        int two=0;
+
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0)zero++;
+            else if(nums[i]==1)one++;
+            else two++;
+        }
+        int j=0;
+        while(zero>0){
+            nums[j]=0;
+            j++;
+            zero--;
+        }
+        while(one>0){
+            nums[j]=1;
+            j++;
+            one--;
+        }
+        while(two>0){
+            nums[j]=2;
+            j++;
+            two--;
+        }
+
+    }
+};
