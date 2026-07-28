d8b558237d1037e261480a1da112fcd0e3441d6a
Sambram S S <sambramss7@gmail.com>
2026-07-28 21:04:52 +0530

    Time: 28 ms (72.09%), Space: 54.2 MB (88.13%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -2,12 +2,23 @@ class Solution {
 public:
     string smallestPalindrome(string s) {
         int n=s.size();
-        string res="";
-        
-        sort(s.begin(),s.begin()+n/2);
-        sort(s.end()-n/2,s.end());
-        reverse(s.end()-n/2,s.end());
+        vector<int> freq(26,0);
 
+        for(int i=0;i<n/2;i++){
+            freq[s[i]-'a']++;
+        }
+
+        int idx=0;
+        for(int i=0;i<26;i++){
+            while(freq[i]-- > 0){
+                s[idx++]=(char)(i+'a');
+            }
+        }
+
+        for(int i=0;i<n/2;i++){
+            s[n-1-i] = s[i];
+        }
+        
         return s;
     }
 };
