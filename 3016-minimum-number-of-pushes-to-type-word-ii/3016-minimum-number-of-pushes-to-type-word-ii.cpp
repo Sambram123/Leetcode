74a0b503f053174da529fa2183649a834a066abc
Sambram S S <sambramss7@gmail.com>
2026-07-31 20:08:12 +0530

    Time: 6 ms (75.61%), Space: 28.4 MB (25.39%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int n=word.length();
+        vector<int> freq(26,0);
+        for(char ch:word){
+            freq[ch-'a']++;
+        }
+        sort(freq.rbegin(),freq.rend());
+
+        int ans=0;
+        for(int i=0;i<26;i++){
+            ans+=((i/8)+1)*freq[i];
+        }
+        return ans;
+    }
+};
